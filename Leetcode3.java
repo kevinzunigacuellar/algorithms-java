be1ec3843eeeb72dee4ac0bd802d25add835c8fe
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-23 00:28:50 -0400

    sliding window technique

---
+++ Leetcode3.java
@@ -0,0 +1,12 @@
+/* 
+Sliding window:
+The sliding window technique is used to find a consecutive subarray or substring.
+It may have a fixed size or variable size.
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+
+public class Leetcode3 {
+  public static void main(String[] args) {
+  }
+}
